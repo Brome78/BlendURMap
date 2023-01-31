3f3e06f7c802ca68fc308800fd49ee58772abe78
Antoine THOMAS <antoine.thomas@epita.fr>
2023-01-31 02:04:22 +0100

    started the main fonction + made a real convesion from bmp to png

--- Generation/main.c
+++ Generation/main.c
@@ -4,25 +4,40 @@
 #include "Noise/simplex.h"
 #include "Color/chunk.h"
 #include "Noise/seed.h"
+#include <err.h>
+#include <png.h>
 #include <stdlib.h>
+#include <libpng/png.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char** argv)
 {
 
+    if(argc != 3 && argc != 2)
+        errx(EXIT_FAILURE,"USAGE: ./main + --perlin or --simplex + (optional) --map");
 
     char *s = read_seed("seed");
+
+    if(strcmp(argv[1],"--simplex") == 0)
+    {
+        
+        SDL_Surface* simplex = generate_simplex(1920,1080,500);
+        save_image(simplex,"simplex.bmp");
+        bmp_to_png("simplex.bmp","simplex.png");
+        return 0;
+    }
+
 
     SDL_Surface* perlin = generate_simplex(1920,1080,50);
     //FILE *seed = fopen("seed","w");
     //fprintf(seed,"%s",perlin->seed);
-    save_image(perlin,"simplex.png");
+    save_image(perlin,"simplex.bmp");
     //struct map *perlin2 = perlin_generate(1920,1080,900,s);
     SDL_Surface *map = apply_biome(perlin, perlin,
             1920,1080);
 
 
-    save_image(map,"map.png");
+    save_image(map,"map.bmp");
 
     //struct chunk **chunk_map = define_chunk(perlin->map,
      //       perlin2->map,1920,1080);
