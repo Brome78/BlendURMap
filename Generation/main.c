9dd70db9f15b66d956e443def10ce39ab951ed6f
thomas.marmaroli <thomas.marmaroli@epita.fr>
2023-01-31 13:15:48 +0100

    end merge

---
+++ Generation/main.c
@@ -0,0 +1,61 @@
+#include "Utils/utils.h"
+#include "Noise/perlin.h"
+#include "Color/color.h"
+#include "Noise/simplex.h"
+#include "Color/chunk.h"
+#include "Noise/seed.h"
+
+#include "Color/props.h"
+
+#include <err.h>
+#include <png.h>
+
+#include <stdlib.h>
+#include <libpng/png.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+
+    if(argc != 3 && argc != 2)
+        errx(EXIT_FAILURE,"USAGE: ./main + --perlin or --simplex + (optional) --map");
+
+    char *s = read_seed("seed");
+
+    if(strcmp(argv[1],"--simplex") == 0)
+    {
+        
+        SDL_Surface* simplex = generate_simplex(1920,1080,500);
+        save_image(simplex,"simplex.bmp");
+        bmp_to_png("simplex.bmp","simplex.png");
+        return 0;
+    }
+
+
+    struct map* perlin = perlin_generate(1920,1080,500,NULL);
+    //FILE *seed = fopen("seed","w");
+    //fprintf(seed,"%s",perlin->seed);
+    save_image(perlin->map,"perlin.bmp");
+    struct map *perlin2 = perlin_generate(1920,1080,900,NULL);
+    SDL_Surface *map = apply_biome(perlin->map, perlin2->map,
+            1920,1080);
+
+
+    draw_bush(map,10,10,1920,1080);
+    save_image(map,"map.bmp");
+    struct chunk **chunk_map = define_chunk(perlin->map,
+            perlin2->map,1920,1080);
+
+
+    save_image(map,"map.bmp");
+
+    //struct chunk **chunk_map = define_chunk(perlin->map,
+     //       perlin2->map,1920,1080);
+
+    //print_chunk(chunk_map, 1000,1000);
+    //map = draw_riviere(map,perlin,1920,1080);
+    //save_image(map,"riviere.png");
+    SDL_FreeSurface(perlin->map);
+    SDL_FreeSurface(map);
+    return 0;
+}
