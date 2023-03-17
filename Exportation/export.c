2c54ba64d203955149974707a34efbe443e8433a
alexandre.leclaire <alexandre.leclaire@epita.fr>
2023-03-17 19:33:38 +0100

    pushing export workspace - might need change to exec_map or similar methods to allow surface to be used by export methods before freeing.

---
+++ Exportation/export.c
@@ -0,0 +1,26 @@
+#include "export.h"
+#include "../Generation/Options/exec_map.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int create_3D_map(SDL_Surface* planemap)
+{
+  //TODO
+  //- Get the map from the generation
+  //- Run exec_map
+  //- Get the surface and the measure of heights of pixels
+  //- Create the .OBJ file
+  //- Write into it with the info
+  //- Don't forget to free the pointers if needed
+
+
+  return 1;
+}
+
+int main()
+{
+  //TODO
+  //Call create_3D_map() and thats it !
+  return 1;
+}
---
+++ Exportation/export.h
@@ -0,0 +1,6 @@
+//this should be the include to get the different biomes/pixels on the map
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+int create_3D_map(SDL_Surface* planemap);
