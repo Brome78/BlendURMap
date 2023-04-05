7b233496ec60bc22a72d366a0d17e9938d38cd16
alexandre.leclaire <alexandre.leclaire@epita.fr>
2023-04-05 21:43:53 +0200

    UI pushed, modified .glade and ui.c

--- UI/ui.c
+++ UI/ui.c
@@ -1,24 +1,36 @@
-#include "../Generation/Utils/utils.h"
+//#include "../Generation/main.h" - placeholder, make header file for main so can be called for gtk
 #include <gtk/gtk.h>
+#include "resize.h"
 
+#include <stdlib.h>²
+#include "../Generation/Options/exec_perso.h"
 
-static gboolean render(GtkImage *image, GdkGLContext *context)
+typedef struct UserInterface
 {
-  // inside this function it's safe to use GL; the given
-  // GdkGLContext has been made current to the drawable
-  // surface used by the `GtkGLArea` and the viewport has
-  // already been set to be the size of the allocation
+  GtkWindow* window;
+  GtkImage* render2d;
+  GtkButton* generate_button;
+  GtkButton* load_button;
+} UserInterface;
+
+typedef struct App
+{
+    UserInterface ui;
+} App;
+
+void on_generate_button_clicked(GtkButton *button, gpointer user_data)
+{
+
+  App *app = user_data; //recover App information
+
   GError *err = NULL;
-  GdkPixbuf* pxbuf = gdk_pixbuf_new_from_file("map.png",&err);
-  gtk_image_set_from_pixbuf(image,pxbuf);
 
-  // draw your object
-  // draw_an_object ();
+  system("feh map.png");
 
-  // we completed our drawing; the draw commands will be
-  // flushed at the end of the signal emission chain, and
-  // the buffers will be drawn on the window
-  return TRUE;
+
+  //resize("map.png","tmp.png",400,400);
+  //GdkPixbuf* pxbuf = gdk_pixbuf_new_from_file("tmp.png",&err);
+  //gtk_image_set_from_pixbuf(app->ui.render2d,pxbuf); //sets the image on the window
 }
 
 int main()
@@ -38,18 +50,34 @@ int main()
     return 1;
   }
 
+
+
   GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "org.gtk.NewApp"));
   //GtkGLArea* render3d = GTK_GL_AREA(gtk_builder_get_object(builder,"3D_window"));
   //GtkButton* start_button = GTK_BUTTON(gtk_builder_get_object(builder, "start_button"));
   GtkImage* render2d = GTK_IMAGE(gtk_builder_get_object(builder, "2D_render"));
   GtkButton* generate_button = GTK_BUTTON(gtk_builder_get_object(builder,"generate"));
+  GtkButton* load_button = GTK_BUTTON(gtk_builder_get_object(builder,"load"));
 
-  // Connects signal handlers.
+
+  App app =
+    {
+        .ui =
+            {
+                .window = window,
+                .render2d = render2d,
+                .load_button = load_button,
+                .generate_button = generate_button,
+            },
+    };
+
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-  g_signal_connect(render2d, "render2d", G_CALLBACK(render), NULL);
-  //g_signal_connect(generate, "generate", G_CALLBACK(render),NULL);
+  //g_signal_connect(render2d, "render2d", G_CALLBACK(render), NULL);
+  g_signal_connect(generate_button, "clicked", G_CALLBACK(on_generate_button_clicked),&app);
   //g_signal_connect(area, "draw", G_CALLBACK(on_draw), &rect);
   //g_signal_connect(area, "configure", G_CALLBACK(on_configure), &rect);
+
   gtk_main();
+
   return 0;
 }
